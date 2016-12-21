76d86a792cec65c830d80779729ed6318528351d
Metta Ong <ongspxm@gmail.com>
2016-12-21 10:43:13 +0800

    fixed css single line comment

--- lib/cssprefix/cssprefix.py
+++ lib/cssprefix/cssprefix.py
@@ -1,13 +1,41 @@
 import rules
 
+def getIdx(text, substring):
+    try:
+        return text.index(substring)
+    except ValueError:
+        return -1
+
 def getRules(text):
     rules = []
-    inner = text.split(';')
-    for rule in inner:
-        if not rule.count(':'): continue
-        rule = map(lambda x:x.strip(), rule.split(':'))
-        rules.append([rule[0], ':'.join(rule[1:])])
 
+    while text:
+        i = getIdx(text, ':')
+        name = text[:i].strip()
+        text = text[i+1:]
+        
+        a = getIdx(text, ':')
+        b = getIdx(text, ';')
+
+        if b<0:
+            raise Exception('error in phasing, missing ";"')
+        elif a<0:
+            value = ';'.join(text.split(';')[:-1]).strip()
+            text = ''
+        elif a>b and not text.split(';')[0].count('url('):
+            temp = text.split(';') 
+            value = temp[0].strip()
+            text = ';'.join(temp[1:])
+        else:
+            temp = text.split(')')
+            value = (temp[0]+')').strip()
+
+            text = ')'.join(temp[1:])
+            text = ';'.join(text.split(';')[1:]) 
+
+        if name and value:
+            rules += [[name, value]]
+    
     return rules
 
 def getBlock(text):
@@ -21,18 +49,14 @@ def getBlock(text):
     )
 
 def getStyles(content):
-    styles = []
-    
-    ### remove comments
-    while content.count('/*'):
-        a = content.index('/*')
-        b = content.index('*/')
-        content = content[:a] + content[b+2:]
+    styles = [] 
+    content = preProcess(content)
 
     while content.count('{'):
         info = getBlock(content)
-
-        if info['name'].startswith('@media'):
+        
+        special = '@media @keyframes'.split()
+        if sum([1 for name in special if info['name'].startswith(name)]):
             wrapper = []
             content = content[content.index('{')+1:]
 
@@ -105,15 +129,44 @@ def processBlock(block):
             rules2 += rules.process(rule)
         block['rules'] = rules2
         return block
+    
+    if not block.get('wrapper'):
+        print block
 
     wrapper = []
     for sublock in block['wrapper']: 
         wrapper.append(processBlock(sublock))
     block['wrapper'] = wrapper
+    
     return block
         
+def preProcess(content):
+    ### remove trailing
+    content = '\n'.join(map(lambda x: x.strip(), content.split('\n')))
 
-def process(content, minify=False):
+    ### remove multiline comments
+    while content.count('/*'):
+        a = content.index('/*')
+        b = content.index('*/')
+        content = content[:a] + content[b+2:]
+   
+    ### remove comments
+    lines = content.split('\n')
+    for i in range(len(lines)):
+        line = lines[i]
+        
+        idx = line.rfind('//')
+        
+        ### dun remove :// links
+        if idx==-1 or line[idx-1]==':':
+            continue
+
+        lines[i] = line[:idx]
+    content = '\n'.join(lines)
+
+    return content
+
+def process(content, minify=False):  
     css = getStyles(content)
     css2 = []
     for block in css:
