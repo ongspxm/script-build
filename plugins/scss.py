d0bf243a1ec60325a864cb4ff4239298b5245519
Metta Ong <ongspxm@gmail.com>
2016-12-21 11:16:23 +0800

    fixed 3efc784b css unicode content error

--- plugins/scss.py
+++ plugins/scss.py
@@ -2,8 +2,11 @@ import scss
 import cssprefix
 
 def extScss(content):
-    content = scss.Compiler().compile_string(content).replace('\\\\', '\\')
-    return cssprefix.process(content, minify=True) 
+    content = scss.Compiler(search_path=['./src']).compile_string(content)
+
+    ### allow unicode characters in content field - content:'\236a'
+    content = content.encode('unicode_escape').replace('\\u', '\\')
+    return cssprefix.process(content.decode('unicode_escape'), minify=True)
 
 def init(funcs, convs):
     funcs['scss'] = extScss
